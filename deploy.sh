175b8ad3101ce180a1109f2c1079ed5c4a38358a
Wai Hon Law <whhone@gmail.com>
2020-04-22 18:14:12 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
+#!/bin/sh
+#
 # Build and upload the site.
+# See https://gohugo.io/hosting-and-deployment/deployment-with-rsync/
 
 OUTPUT_DIR=/tmp/whhone/public/
 
-rm -rf ${OUTPUT}
+rm -rf ${OUTPUT_DIR}
 
-hugo -d ${OUTPUT_DIR} && rsync -aP --delete ${OUTPUT_DIR} whhone.com:/var/www/whhone.com/
+hugo -d ${OUTPUT_DIR} && rsync -avzP --delete ${OUTPUT_DIR} whhone.com:/var/www/whhone.com/
